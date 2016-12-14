8ff18952b72ff4e0ba6a04b6334bbfe30783461f
tortuepin <kameyasu119@gmail.com>
2016-12-14 10:47:25 +0900

    tf-idf

---
+++ tf-idf.rb
@@ -0,0 +1,61 @@
+def getWord(w)
+    return w.split(" ")[0]
+end
+
+def makeIdf(filenames)
+    idf = {}
+    filenames.each{|filename|
+        f = open(filename)
+        tmpIdf = []
+        while tmp = f.gets
+            word = getWord(tmp)
+            if idf.has_key?(word)
+                if !tmpIdf.include?(word)
+                    idf[word] += 1
+                    tmpIdf.push(word)
+                end
+            else
+                idf.store(word, 1)
+                if !tmpIdf.include?(word)
+                    tmpIdf.push(word)
+                end
+            end
+        end
+        f.close
+    }
+    #ここまででdf値がもとまる
+    
+
+    #idfをもとめる
+    n = filenames.length
+
+    idf.each_key do |key|
+#        print(idf[key], ":", key, "\n")
+        dftmp = n/idf[key] 
+        dftmp = Math.log(dftmp) + 1
+        idf[key] = dftmp
+    end
+    return idf
+end
+
+
+
+wordArr = []
+wei = []
+
+filenames = ["text.txt", "text2.txt"]
+idf = makeIdf(filenames)
+while tmp = gets do
+    word = getWord(tmp)
+    if !wordArr.include?(word) then
+        wordArr.push(word)
+        wei.push(idf[word])
+    else
+        wei[wordArr.index(word)] += idf[word]
+    end
+end
+
+
+for i in 0..wordArr.length-1 do
+    print(wordArr[i], ":", wei[i], "\n")
+end
